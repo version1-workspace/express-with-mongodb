dd90b8acfe7c0b5bff0c55138e0f8fdc5975cc3c
akaaariii <caaari23@gmail.com>
2021-01-20 03:46:17 -0800

    add MVC model and connect to mongoose

---
+++ model/Articles.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
